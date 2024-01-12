bee1b9a4914a2821ed459b16ea30d17016914a86
Thomas Heede <145970483+ThomasHeede@users.noreply.github.com>
2024-01-12 14:29:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import numpy as np
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn import metrics
+from six import StringIO
+from IPython.display import Image
+import pydotplus

40a8620476cd319cdd56fe781b6a3a50281dc5fa
Thomas Heede <145970483+ThomasHeede@users.noreply.github.com>
2024-01-12 14:27:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import numpy as np
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
